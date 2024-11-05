a6762960ee50f0dbcc6625824f5a493234fba6c6
Bryan Mogens Warren <bryanwarren86@gmail.com>
2024-11-05 20:46:16 +0700

    Initial commit

---
+++ app/src/main/java/paba/coba/recycleview/MainActivity.kt
@@ -0,0 +1,64 @@
+package paba.coba.recycleview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var _nama: Array<String>
+    private lateinit var _karakter: Array<String>
+    private lateinit var _deskripsi: Array<String>
+    private lateinit var _gambar: Array<String>
+
+    private var arrWayang = arrayListOf<wayang>()
+
+    private lateinit var _rvWayang: RecyclerView
+
+    fun siapkanData() {
+        _nama = resources.getStringArray(R.array.namaWayang)
+        _karakter = resources.getStringArray(R.array.karakterUtamaWayang)
+        _deskripsi = resources.getStringArray(R.array.deskripsiWayang)
+        _gambar = resources.getStringArray(R.array.gambarWayang)
+    }
+
+    fun tambahData() {
+        for (position in _nama.indices) {
+            var data = wayang(
+                _gambar[position],
+                _nama[position],
+                _karakter[position],
+                _deskripsi[position]
+            )
+            arrWayang.add(data)
+        }
+    }
+
+    fun tampilkanData() {
+//        _rvWayang.layoutManager = LinearLayoutManager(this)
+//        _rvWayang.layoutManager = GridLayoutManager(this, 2)
+        _rvWayang.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        _rvWayang.adapter = adapterRecView(arrWayang)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        _rvWayang = findViewById<RecyclerView>(R.id.rvWayang)
+        siapkanData()
+        tambahData()
+        tampilkanData()
+    }
+}
---
+++ app/src/main/java/paba/coba/recycleview/wayang.kt
@@ -0,0 +1,8 @@
+package paba.coba.recycleview
+
+data class wayang(
+    var foto : String,
+    var nama : String,
+    var karakter : String,
+    var deskripsi : String
+)
